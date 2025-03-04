19450d70ecb52a7e6b49caf504dd9fac7c90e07b
Yoel <yoeldelbuono@outlook.com>
2025-03-04 17:56:07 -0300

    refactor health

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
-
 app.get('/health', (req, res) => {
   res.send('ok')
 })
+
+app.use(express.static('dist'))
+
 
 app.listen(PORT, () => {
   /* eslint-disable-next-line no-console */
