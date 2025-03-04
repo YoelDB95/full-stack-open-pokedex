a516e7a9de61f3d14d5804b8f78113b039204c60
Yoel <yoeldelbuono@outlook.com>
2025-03-04 18:35:11 -0300

    error deleted

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
